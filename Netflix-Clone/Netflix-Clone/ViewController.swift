0fe4ffba451aff3b6d6cf85c48ca6d55c8a5ae8d
Muhammed Enes <kiliclimuhammedenes@gmail.com>
2023-02-22 12:12:24 +0300

    last changes

---
+++ Netflix-Clone/Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by Muhammed Enes Kılıçlı on 22.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    let build = Int() 
+
+}
+
